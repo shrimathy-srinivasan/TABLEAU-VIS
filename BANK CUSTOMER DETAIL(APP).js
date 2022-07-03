1bd8c3cb3d63c8d284406bd5e285d29627eea380
Shrimathy Srinivasan <108602440+shrimathy-srinivasan@users.noreply.github.com>
2022-07-03 11:07:10 +0530

    Update BANK CUSTOMER DETAIL(APP).js

--- BANK CUSTOMER DETAIL(APP).js
+++ BANK CUSTOMER DETAIL(APP).js
@@ -3,7 +3,7 @@ console.log('Is this working?');
 let viz;
 
 //Add Share Link to Tableau Public in here
-const url = https://public.tableau.com/views/section61_16553832899720/Story1?:language=en-US&:display_count=n&:origin=viz_share_link ;
+const url = https://public.tableau.com/views/DASHBOARD-UKBANK/Dashboard1?:language=en-US&:display_count=n&:origin=viz_share_link ;
 
 const vizContainer = document.getElementById('vizContainer');
 const options = {
